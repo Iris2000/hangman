42ef5464b66086a080f1a9d2dc1c740934f93e1f
Iris2000 <43512172+Iris2000@users.noreply.github.com>
2020-02-29 14:17:40 +0800

    testing commit

---
+++ hangman.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << endl;
+    cout << "Enter your age: ";
+    cin >> age;
+    return 0;
+}
