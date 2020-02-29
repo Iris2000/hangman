e1b3ee1da5546c9599e93440c005d442f53eaa0b
Iris2000 <43512172+Iris2000@users.noreply.github.com>
2020-02-29 23:17:25 +0800

    menu done

--- hangman.cpp
+++ hangman.cpp
@@ -1,14 +1,100 @@
 #include <iostream>
 using namespace std;
 
+// function declaration
+void welcome();
+int menu();
+void startGame();
+void leaderboard();
+void quitGame();
+
+void welcome() 
+{
+    cout<<"\n\n";
+    cout<<"\t\t\t\t\t***************************************************************************************\n";
+    cout<<"\t\t\t\t\t**                                           		                             **\n";
+    cout<<"\t\t\t\t\t**                                            		                             **\n";
+    cout<<"\t\t\t\t\t**                                            		                             **\n";
+    cout<<"\t\t\t\t\t**                                            		                             **\n";
+    cout<<"\t\t\t\t\t**                                            		                             **\n";
+    cout<<"\t\t\t\t\t**                                            		                             **\n";
+    cout<<"\t\t\t\t\t**                                  WELCOME TO                                       **\n";
+    cout<<"\t\t\t\t\t**                                                                                   **\n";
+    cout<<"\t\t\t\t\t**                             HANGMAN GUESSING GAME                                 **\n";
+    cout<<"\t\t\t\t\t**                                                                                   **\n";
+    cout<<"\t\t\t\t\t**                                                                                   **\n";
+    cout<<"\t\t\t\t\t**                                                                                   **\n";
+    cout<<"\t\t\t\t\t**                                                                                   **\n";
+    cout<<"\t\t\t\t\t**                                                           -Brought To You by GIT  **\n";
+    cout<<"\t\t\t\t\t**                                                                                   **\n";
+    cout<<"\t\t\t\t\t***************************************************************************************\n\n\n\n\t\t\t\t\t";
+    system("pause");
+    system("cls");
+}
+
+int menu()
+{
+    int choice;
+    cout<<"\n\n\t\t\t\t\t\t\t\t    HANGMAN GUESSING GAME MENU\n";	
+    cout<<"\n\n\t\t\t\t\t\t\t\tChoose from the following Options:\n\n";
+    cout<<"\t\t\t\t\t\t\t _____________________________________________________ \n";
+    cout<<"\t\t\t\t\t\t\t|                                        	      |\n";
+    cout<<"\t\t\t\t\t\t\t|             1  >> Start game                        |\n";
+    cout<<"\t\t\t\t\t\t\t|             2  >> View Leaderboard                  |\n";
+    cout<<"\t\t\t\t\t\t\t|             3  >> Exit the Program                  |\n";
+    cout<<"\t\t\t\t\t\t\t|_____________________________________________________|\n\n";
+    cout<<"\t\t\t\t\t\t\tEnter your choice: ";
+    cin>>choice;
+    cin.clear();
+    cin.ignore();
+
+    while(choice>3||choice<1)
+    {
+        cout<<"\n\t\t\t\t\t\t\tInvalid Choice\n";
+        cout<<"\t\t\t\t\t\t\tTry again...........\n\n";
+        cout<<"\t\t\t\t\t\t\tEnter your choice: ";
+        cin>>choice;
+        cin.clear();
+        cin.ignore();
+    }
+
+    return choice;
+}
+
+void startGame()
+{
+    cout << "Welcome to HANGMAN GUESSING GAME" << endl;
+}
+
+void leaderboard()
+{
+    cout << "This is leaderboard" << endl;
+}
+
+void quitGame()
+{
+    cout << "Thank you for your support." << endl;
+}
+
 int main()
 {
-    string name;
-    int age;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello, " << name << endl;
-    cout << "Enter your age: ";
-    cin >> age;
-    return 0;
+   int choice; 
+
+   welcome();
+   choice = menu();
+
+   if(choice == 1)
+   {
+       startGame();
+   }
+   else if (choice == 2)
+   {
+       leaderboard();
+   }
+   else
+   {
+       quitGame();
+   }
+
+   return 0;
 }
